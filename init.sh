7c191f3ed6415515a99c0f84e91ca7aa2d3d16df
sdiadech <stasdyad@gmail.com>
2017-05-08 18:19:44 +0300

    nginx config

--- init.sh
+++ init.sh
@@ -1,2 +1,17 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+#sudo /etc/init.d/nginx restart
+
+mkdir -p /home/box/web/etc/
+mkdir -p /home/box/web/uploads/
+mkdir -p /home/box/web/public/css/
+mkdir -p /home/box/web/public/img/
+mkdir -p /home/box/web/public/js/
+
+# Configure NGINX
+sudo rm -rf /etc/nginx/nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+cp nginx.conf /home/box/web/etc
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
+
+# Run NGINX
 sudo /etc/init.d/nginx restart
