ae978b45a6d539530b36b82be4d28ca1aba080a5
sdiadech <stasdyad@gmail.com>
2017-05-15 22:52:56 +0300

    django configuration

--- conf/ask.py
+++
@@ -1,3 +0,0 @@
-bind = '0.0.0.0:8000'
-errorlog = '/home/box/web/gunicorn.log'
-loglevel = 'warning'
--- init.sh
+++ init.sh
@@ -19,12 +19,12 @@ sudo /etc/init.d/nginx restart
 # Configure Gunicorn and echo WSGI server
 #cp hello.py /home/box/web/
 cp ./conf/hello.py /home/box/web/etc/
-cp ./conf/ask.py /home/box/web/etc/
+cp ./conf/ask.conf /home/box/web/etc/
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -sf /home/box/web/etc/ask.conf /etc/gunicorn.d/ask
 
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -c /home/box/web/etc/hello.py hello:application
-sudo gunicorn -c /home/box/web/etc/ask.py ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/ask.conf
 #sudo gunicorn -b 0.0.0.0:8080 hello:application
