2c6ff1d872fb2a87934029be4090f67ed080e667
sdiadech <stasdyad@gmail.com>
2017-05-18 20:56:47 +0300

    django configuration

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Question(models.Model):
-    title = models.CharField(max_length=50)
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
-    author = models.ForeignKey(User, related_name='question_author')
-    likes = models.ManyToManyField(User, related_name='question_likes')
-
-    def __unicode__(self):
-        return self.title
-
-
 class QuestionManager(models.Manager):
 
     def new(self):
@@ -21,6 +9,19 @@ class QuestionManager(models.Manager):
 
     def popular(self):
         return Question.objects.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name='question_author')
+    likes = models.ManyToManyField(User, related_name='question_likes')
+    objects = QuestionManager()
+
+    def __unicode__(self):
+        return self.title
 
 
 class Answer(models.Model):
